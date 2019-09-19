ad85887762199c0f5b069ae2027edab94abfeb90
kanchan0 <coolkanchan13@gmail.com>
2019-09-19 15:59:41 +0530

    changing modularity

--- server.js
+++ server.js
@@ -1,73 +1,19 @@
-const app = require("express")()
-var http = require('http').createServer(app);
-const mongo = require("mongodb").MongoClient;
-const client = require('socket.io').listen(4000).sockets;
+const app               =       require("express")()
+const bodyParser        =       require("body-parser")
+var http                =       require('http').createServer(app);
+const driverfunction    =       require("./modals")
 
-//connect to mongo
-mongo.connect('mongodb://127.0.0.1/mongochat',{ useNewUrlParser: true,useUnifiedTopology: true },function(err,cl){
-    if(err){
-       
-        throw err;
-    }
-    console.log("MongoDB connected ...");
-    const db = cl.db("mongochat");
-
-
-    //connect to socket.io
-    client.on('connection',function(socket){
-            let chat = db.collection('chats');
-            
-            //function to send status
-            sendStatus = function(s){
-                socket.emit("status",s)
-            }
-
-            //get chats from mongo collection
-            chat.find().limit(100).sort({_id:1}).toArray(function(err,res){
-                if(err){
-                    throw err;
-                }
-                //emit the messages
-                socket.emit("output",res)
-            });
-
-
-            //handle input events
-            socket.on('input',function(data){
-                let name = data.name;
-                let message = data.message;
-            
-            //check for name and message
-            if(name==""||message==""){
-                //send error status
-                sendStatus("please ,enter the name and message");
-            }else{
-                //insert message
-                chat.insert({name:name,message:message},function(){
-                client.emit("output",[data])
-
-                //send status object
-                 sendStatus({
-                       message:"message sent",
-                        clear:true
-                    })
-                })
-            }
-        });
-        socket.on('clear',function(){
-            //remove all chats from collection
-            chat.remove({},function(){
-                socket.emit('cleared')
-            })
-        })
-    })
-})
-
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  
+app.use(bodyParser.json());
 
 app.get('/chat', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
 
+driverfunction();
 http.listen(5500, function(){
     console.log('listening on *:5500');
   });
